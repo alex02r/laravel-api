1bed7c4b2112907917771ce32982e3f5cdc4e004
alex02r <alex.giochi02@gmail.com>
2024-03-04 13:30:23 +0100

    [UPGRADE] Visualizzazione dei progetti in tabella

--- resources/views/admin/projects/index-project.blade.php
+++ resources/views/admin/projects/index-project.blade.php
@@ -12,27 +12,65 @@
                     </div>
                 </div>
             </div>
-            @foreach ($projects as $project)
-                <div class="col-3">
-                    <div class="card shadow h-100">
-                        <img class="card-img-top p-3" src="{{ $project->img !== null ? asset('storage/'. $project->img) : 'https://cdn-icons-png.flaticon.com/512/3767/3767084.png' }}" alt="img project">
-                        <div class="card-body">
-                          <h5 class="card-title">{{ $project->name}}</h5>
-                          <div class="text-secondary">Data Creazione: {{ $project->start_date }}</div>
-                          <div class="text-secondary">Data Fine: {{ $project->end_date }}</div>
-                          <div class="mt-2 d-flex gap-2">
-                            <a href="{{ route('admin.project.show', ['project' => $project]) }}" class="btn btn-sm btn-primary">Visualizza</a>
-                            <a href="{{ route('admin.project.edit', ['project'=>$project]) }}" class="btn btn-sm btn-warning">Edit</a>
-                            <form action="{{ route('admin.project.destroy', ['project'=>$project]) }}" method="post" onsubmit="return confirm('Sei sicuro di voler eliminare questo project')">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-sm btn-danger">Elimina</button>
-                            </form>
-                          </div>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+            <div class="col-12">
+                <table class="table">
+                    <thead>
+                        <th>Nome</th>
+                        <th>Tipologia</th>
+                        <th>Tecnologia</th>
+                        <th>Data Creazione</th>
+                        <th>Data Fine</th>
+                        <th>Strumenti</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($projects as $project)
+                            <tr>
+                                <td>{{ $project->name}}</td>
+                                <td>{{ $project->type ? $project->type->name : 'Nessun tipo'}}</td>
+                                <td>
+                                    @forelse ($project->technologies as $tech)
+                                        <span class="badge rounded-pill text-bg-{{ $tech->class_color}}"> {{ $tech->name }}</span>
+                                    @empty
+                                        Nessuna tecnologia
+                                    @endforelse
+                                </td>
+                                <td>{{ $project->start_date }}</td>
+                                <td>{{ $project->end_date }}</td>
+                                <td>
+                                    <div class="mt-2 d-flex gap-2">
+                                        <a href="{{ route('admin.project.show', ['project' => $project]) }}" class="btn btn-sm btn-primary">Visualizza</a>
+                                        <a href="{{ route('admin.project.edit', ['project'=>$project]) }}" class="btn btn-sm btn-warning">Edit</a>
+                                        <form action="{{ route('admin.project.destroy', ['project'=>$project]) }}" method="post" onsubmit="return confirm('Sei sicuro di voler eliminare questo project')">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-sm btn-danger">Elimina</button>
+                                        </form>
+                                      </div>
+                                </td>
+                            </tr>
+                            {{-- <div class="col-3">
+                                <div class="card shadow h-100">
+                                    <img class="card-img-top p-3" src="{{ $project->img !== null ? asset('storage/'. $project->img) : 'https://cdn-icons-png.flaticon.com/512/3767/3767084.png' }}" alt="img project">
+                                    <div class="card-body">
+                                      <h5 class="card-title">{{ $project->name}}</h5>
+                                      <div class="text-secondary">Data Creazione: {{ $project->start_date }}</div>
+                                      <div class="text-secondary">Data Fine: {{ $project->end_date }}</div>
+                                      <div class="mt-2 d-flex gap-2">
+                                        <a href="{{ route('admin.project.show', ['project' => $project]) }}" class="btn btn-sm btn-primary">Visualizza</a>
+                                        <a href="{{ route('admin.project.edit', ['project'=>$project]) }}" class="btn btn-sm btn-warning">Edit</a>
+                                        <form action="{{ route('admin.project.destroy', ['project'=>$project]) }}" method="post" onsubmit="return confirm('Sei sicuro di voler eliminare questo project')">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-sm btn-danger">Elimina</button>
+                                        </form>
+                                      </div>
+                                    </div>
+                                </div>
+                            </div> --}}
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 @endsection
