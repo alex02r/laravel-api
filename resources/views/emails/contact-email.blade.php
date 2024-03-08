5b9ed0375aa19c11be65d6454fc213ee9b5b11d1
alex02r <alex.giochi02@gmail.com>
2024-03-08 16:25:14 +0100

    [UPGRADE] Creazione pagina messaggio

---
+++ resources/views/emails/contact-email.blade.php
@@ -0,0 +1,10 @@
+<h1>Nuova email</h1>
+<p>
+    Nome: {{ $lead->name }} <br>
+    Cognome: {{ $lead->surname }} <br>
+    Numero di telefono: {{ $lead->phone }} <br>
+    Email: {{ $lead->mail }} <br>
+    Messaggio:<br>
+    {{ $lead->mex }} 
+
+</p>
